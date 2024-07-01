80681e33fe3fe454a72675b8122931c186057a3a
texascake <budalblimbing@gmail.com>
2024-07-01 11:39:25 +0700

    Update file for upload [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/X00T/lineage*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls lineage*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/lineage*.zip asus:lineage -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls lineage*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X00T/lineage-*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip)"
+      rclone copy ~/rom/out/target/product/X00T/lineage-*.zip asus:lineage -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls lineage-*.zip) Uploaded Successfully!"
 fi
