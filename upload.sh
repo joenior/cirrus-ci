f9572edcecfc1ead967146b5dc2f30f9c7d0b434
texascake soft <budalblimbing@gmail.com>
2024-07-13 16:32:08 +0000

    ci: fix error mistake()

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/X00TD/xdroid_*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls xdroid_*.zip)"
-      rclone copy ~/rom/out/target/product/X00TD/xdroid_*.zip asus:xdroid -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00TD/ && ls xdroid_*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/X00TD/xdCLO_*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00TD/ && ls xdCLO_*.zip)"
+      rclone copy ~/rom/out/target/product/X00TD/xdCLO_*.zip asus:xdroid -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00TD/ && ls xdCLO_*.zip) Uploaded Successfully!"
 fi
