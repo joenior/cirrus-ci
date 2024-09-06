9b913e44f274fbe0c44c77d27cd694b7ee716fac
texascake <budalblimbing@gmail.com>
2024-09-06 08:51:43 +0700

    ci: get ccache()

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/X00T/qassa*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/qassa*.zip rve:qassa -P
+      rclone copy ~/rom/out/target/product/X00T/qassa*.zip ewe:qassa -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls qassa*.zip) Uploaded Successfully!"
 fi
