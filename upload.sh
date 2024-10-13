e2dc8a3a50a064da16262bb7de860c1e8ed66fa0
texascake <budalblimbing@gmail.com>
2024-10-13 09:26:07 +0700

    ci: change gdrive [skip ci]

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 if [ -f ~/rom/out/target/product/X00T/Nusantara-*.zip ]; then
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="📤 Uploading Build $(cd ~/rom/out/target/product/X00T/ && ls Nusantara-*.zip)"
-      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip ewe:nad -P
+      rclone copy ~/rom/out/target/product/X00T/Nusantara-*.zip Gdrive:nad -P
       curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="✅ Build $(cd ~/rom/out/target/product/X00T/ && ls Nusantara-*.zip) Uploaded Successfully!"
 fi
